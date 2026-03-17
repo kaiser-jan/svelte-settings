03ddfa2d7a4fcced34eb16637a2345df7e24cbca
Jan Kaiser <75926077+kaiser-jan@users.noreply.github.com>
2026-03-17 18:17:27 +0100

    feat(Item): convert container to label

--- src/lib/components/ui/SettingsItemContainer.svelte
+++ src/lib/components/ui/SettingsItemContainer.svelte
@@ -4,36 +4,29 @@
   import { cn } from '$lib/utils.js'
   import type { Snippet } from 'svelte'
   import type { ButtonProps } from './button/button.svelte'
+  import { type HTMLAttributes } from 'svelte/elements'
 
   interface Props extends ButtonProps {
-    path: readonly string[]
     item: SettingsBlueprintItem
     changed?: boolean
     hideLabel?: boolean
     children: Snippet
   }
 
-  const { item, changed, hideLabel, children, path, class: className, ...props }: Props = $props()
+  const { item, changed, hideLabel, children, class: className, ...props }: Props = $props()
 
   const options = getOptionsContext()
   const { Button } = options.components
 </script>
 
-<Button
-  {...props}
-  variant={options.style.button.category}
-  class={cn(
-    'px-auto relative h-fit min-h-14 flex-wrap justify-between overflow-hidden p-2 text-base whitespace-normal',
-    className,
-  )}
->
+{#snippet content()}
   {#if !hideLabel}
-    <label class="ml-1 flex shrink-0 flex-row items-center gap-3 font-medium" for={path?.join('_')}>
+    <span class="ml-1 flex shrink-0 flex-row items-center gap-3 font-medium">
       {#if item.icon}
         <item.icon class="text-text" />
       {/if}
       {item.label}
-    </label>
+    </span>
   {/if}
 
   {@render children()}
@@ -47,4 +40,29 @@
   {#if changed}
     <span class="absolute top-0 bottom-0 left-0 h-full w-0.5 bg-primary"></span>
   {/if}
-</Button>
+{/snippet}
+
+<!-- NOTE: using a label for e.g. inputs, so the whole item is clickable -->
+{#if 'onclick' in props || 'href' in props}
+  <label
+    class={cn(
+      options.style.category.classes,
+      'px-auto relative h-fit min-h-14 shrink-0 flex-wrap justify-between overflow-hidden p-2 text-base whitespace-normal',
+      className,
+    )}
+    {...props as HTMLAttributes<HTMLLabelElement>}
+  >
+    {@render content()}
+  </label>
+{:else}
+  <Button
+    {...props}
+    variant={options.style.button.category}
+    class={cn(
+      'px-auto relative h-fit min-h-14 shrink-0 flex-wrap justify-between overflow-hidden p-2 text-base whitespace-normal',
+      className,
+    )}
+  >
+    {@render content()}
+  </Button>
+{/if}
